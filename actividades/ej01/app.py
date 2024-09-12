6d7dd3b96647cca3207f96a0e201a0378537758d
marianobianqui <83589062+marianobianqui@users.noreply.github.com>
2024-09-12 13:38:54 -0300

    Update app.py

--- actividades/ej01/app.py
+++ actividades/ej01/app.py
@@ -1,1 +1,12 @@
-# Aplicacion del servidor
+from microdot import Microdot, send_file
+
+app = Microdot()
+
+@app.route('/')
+async def index(request):
+    return send_file('index.html')
+@app.route("/<dir>/<file>")
+async def static(request, dir, file):
+    return send_file("/"+ dir +"/" + file)
+
+app.run(port = 80)
